f9ac791e7888bb08dbe9b8618bdc042340c71a08
Kamil Samigullin <kamil@samigullin.info>
2018-04-30 17:03:03 +0300

    add click completion

---
+++ bash_completion.d/_click.zsh
@@ -0,0 +1,20 @@
+#compdef click
+
+_arguments \
+  '1: :->level1' \
+  '2: :_files'
+case $state in
+  level1)
+    case $words[1] in
+      click)
+        _arguments '1: :(completion help migrate run version)'
+      ;;
+      *)
+        _arguments '*: :_files'
+      ;;
+    esac
+  ;;
+  *)
+    _arguments '*: :_files'
+  ;;
+esac
