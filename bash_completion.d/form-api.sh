9a83c2481f80dcd93d4b4a2b316638851c907a12
Kamil Samigullin <kamil@samigullin.info>
2018-10-17 13:52:14 +0300

    update completion of form-api

--- bash_completion.d/form-api.sh
+++ bash_completion.d/form-api.sh
@@ -107,7 +107,13 @@ __form-api_handle_reply()
     fi
 
     if [[ ${#COMPREPLY[@]} -eq 0 ]]; then
-        declare -F __custom_func >/dev/null && __custom_func
+		if declare -F __form-api_custom_func >/dev/null; then
+			# try command name qualified custom func
+			__form-api_custom_func
+		else
+			# otherwise fall back to unqualified for compatibility
+			declare -F ___custom_func >/dev/null && __custom_func
+		fi
     fi
 
     # available in bash-completion >= 2, not always present on macOS
@@ -229,6 +235,14 @@ __form-api_handle_word()
         __form-api_handle_command
     elif [[ $c -eq 0 ]]; then
         __form-api_handle_command
+    elif __form-api_contains_word "${words[c]}" "${command_aliases[@]}"; then
+        # aliashash variable is an associative array which is only supported in bash > 3.
+        if [[ -z "${BASH_VERSION}" || "${BASH_VERSINFO[0]}" -gt 3 ]]; then
+            words[c]=${aliashash[${words[c]}]}
+            __form-api_handle_command
+        else
+            __form-api_handle_noun
+        fi
     else
         __form-api_handle_noun
     fi
@@ -238,6 +252,9 @@ __form-api_handle_word()
 _form-api_completion()
 {
     last_command="form-api_completion"
+
+    command_aliases=()
+
     commands=()
 
     flags=()
@@ -246,9 +263,196 @@ _form-api_completion()
     flags_with_completion=()
     flags_completion=()
 
+    flags+=("--format=")
+    two_word_flags+=("-f")
+    local_nonpersistent_flags+=("--format=")
     flags+=("--help")
     flags+=("-h")
     local_nonpersistent_flags+=("--help")
+
+    must_have_one_flag=()
+    must_have_one_flag+=("--format=")
+    must_have_one_flag+=("-f")
+    must_have_one_noun=()
+    noun_aliases=()
+}
+
+_form-api_ctl_create()
+{
+    last_command="form-api_ctl_create"
+
+    command_aliases=()
+
+    commands=()
+
+    flags=()
+    two_word_flags=()
+    local_nonpersistent_flags=()
+    flags_with_completion=()
+    flags_completion=()
+
+    flags+=("--dry-run")
+    flags+=("--filename=")
+    two_word_flags+=("-f")
+    flags+=("--grpc-host=")
+    flags+=("--output=")
+    two_word_flags+=("-o")
+    flags+=("--timeout=")
+    two_word_flags+=("-t")
+    flags+=("--token=")
+
+    must_have_one_flag=()
+    must_have_one_noun=()
+    noun_aliases=()
+}
+
+_form-api_ctl_delete()
+{
+    last_command="form-api_ctl_delete"
+
+    command_aliases=()
+
+    commands=()
+
+    flags=()
+    two_word_flags=()
+    local_nonpersistent_flags=()
+    flags_with_completion=()
+    flags_completion=()
+
+    flags+=("--dry-run")
+    flags+=("--filename=")
+    two_word_flags+=("-f")
+    flags+=("--grpc-host=")
+    flags+=("--output=")
+    two_word_flags+=("-o")
+    flags+=("--timeout=")
+    two_word_flags+=("-t")
+    flags+=("--token=")
+
+    must_have_one_flag=()
+    must_have_one_noun=()
+    noun_aliases=()
+}
+
+_form-api_ctl_read()
+{
+    last_command="form-api_ctl_read"
+
+    command_aliases=()
+
+    commands=()
+
+    flags=()
+    two_word_flags=()
+    local_nonpersistent_flags=()
+    flags_with_completion=()
+    flags_completion=()
+
+    flags+=("--dry-run")
+    flags+=("--filename=")
+    two_word_flags+=("-f")
+    flags+=("--grpc-host=")
+    flags+=("--output=")
+    two_word_flags+=("-o")
+    flags+=("--timeout=")
+    two_word_flags+=("-t")
+    flags+=("--token=")
+
+    must_have_one_flag=()
+    must_have_one_noun=()
+    noun_aliases=()
+}
+
+_form-api_ctl_schema()
+{
+    last_command="form-api_ctl_schema"
+
+    command_aliases=()
+
+    commands=()
+
+    flags=()
+    two_word_flags=()
+    local_nonpersistent_flags=()
+    flags_with_completion=()
+    flags_completion=()
+
+    flags+=("--for=")
+    local_nonpersistent_flags+=("--for=")
+    flags+=("--dry-run")
+    flags+=("--filename=")
+    two_word_flags+=("-f")
+    flags+=("--grpc-host=")
+    flags+=("--output=")
+    two_word_flags+=("-o")
+    flags+=("--timeout=")
+    two_word_flags+=("-t")
+    flags+=("--token=")
+
+    must_have_one_flag=()
+    must_have_one_flag+=("--for=")
+    must_have_one_noun=()
+    noun_aliases=()
+}
+
+_form-api_ctl_update()
+{
+    last_command="form-api_ctl_update"
+
+    command_aliases=()
+
+    commands=()
+
+    flags=()
+    two_word_flags=()
+    local_nonpersistent_flags=()
+    flags_with_completion=()
+    flags_completion=()
+
+    flags+=("--dry-run")
+    flags+=("--filename=")
+    two_word_flags+=("-f")
+    flags+=("--grpc-host=")
+    flags+=("--output=")
+    two_word_flags+=("-o")
+    flags+=("--timeout=")
+    two_word_flags+=("-t")
+    flags+=("--token=")
+
+    must_have_one_flag=()
+    must_have_one_noun=()
+    noun_aliases=()
+}
+
+_form-api_ctl()
+{
+    last_command="form-api_ctl"
+
+    command_aliases=()
+
+    commands=()
+    commands+=("create")
+    commands+=("delete")
+    commands+=("read")
+    commands+=("schema")
+    commands+=("update")
+
+    flags=()
+    two_word_flags=()
+    local_nonpersistent_flags=()
+    flags_with_completion=()
+    flags_completion=()
+
+    flags+=("--dry-run")
+    flags+=("--filename=")
+    two_word_flags+=("-f")
+    flags+=("--grpc-host=")
+    flags+=("--output=")
+    two_word_flags+=("-o")
+    flags+=("--timeout=")
+    two_word_flags+=("-t")
+    flags+=("--token=")
 
     must_have_one_flag=()
     must_have_one_noun=()
@@ -258,6 +462,9 @@ _form-api_completion()
 _form-api_migrate()
 {
     last_command="form-api_migrate"
+
+    command_aliases=()
+
     commands=()
 
     flags=()
@@ -266,31 +473,24 @@ _form-api_migrate()
     flags_with_completion=()
     flags_completion=()
 
-    flags+=("--db_host=")
-    local_nonpersistent_flags+=("--db_host=")
-    flags+=("--db_idle_conn=")
-    local_nonpersistent_flags+=("--db_idle_conn=")
-    flags+=("--db_name=")
-    local_nonpersistent_flags+=("--db_name=")
-    flags+=("--db_open_conn=")
-    local_nonpersistent_flags+=("--db_open_conn=")
-    flags+=("--db_pass=")
-    local_nonpersistent_flags+=("--db_pass=")
-    flags+=("--db_port=")
-    local_nonpersistent_flags+=("--db_port=")
-    flags+=("--db_ssl_mode=")
-    local_nonpersistent_flags+=("--db_ssl_mode=")
-    flags+=("--db_timeout=")
-    local_nonpersistent_flags+=("--db_timeout=")
-    flags+=("--db_user=")
-    local_nonpersistent_flags+=("--db_user=")
+    flags+=("--db-conn-max-lt=")
+    local_nonpersistent_flags+=("--db-conn-max-lt=")
+    flags+=("--db-idle-conn=")
+    local_nonpersistent_flags+=("--db-idle-conn=")
+    flags+=("--db-open-conn=")
+    local_nonpersistent_flags+=("--db-open-conn=")
     flags+=("--dry-run")
     local_nonpersistent_flags+=("--dry-run")
+    flags+=("--dsn=")
+    local_nonpersistent_flags+=("--dsn=")
     flags+=("--limit=")
+    two_word_flags+=("-l")
     local_nonpersistent_flags+=("--limit=")
     flags+=("--schema=")
+    two_word_flags+=("-s")
     local_nonpersistent_flags+=("--schema=")
     flags+=("--table=")
+    two_word_flags+=("-t")
     local_nonpersistent_flags+=("--table=")
     flags+=("--with-demo")
     local_nonpersistent_flags+=("--with-demo")
@@ -303,6 +503,9 @@ _form-api_migrate()
 _form-api_run()
 {
     last_command="form-api_run"
+
+    command_aliases=()
+
     commands=()
 
     flags=()
@@ -313,32 +516,28 @@ _form-api_run()
 
     flags+=("--base-url=")
     local_nonpersistent_flags+=("--base-url=")
-    flags+=("--bind=")
-    local_nonpersistent_flags+=("--bind=")
     flags+=("--cpus=")
+    two_word_flags+=("-C")
     local_nonpersistent_flags+=("--cpus=")
-    flags+=("--db_host=")
-    local_nonpersistent_flags+=("--db_host=")
-    flags+=("--db_idle_conn=")
-    local_nonpersistent_flags+=("--db_idle_conn=")
-    flags+=("--db_name=")
-    local_nonpersistent_flags+=("--db_name=")
-    flags+=("--db_open_conn=")
-    local_nonpersistent_flags+=("--db_open_conn=")
-    flags+=("--db_pass=")
-    local_nonpersistent_flags+=("--db_pass=")
-    flags+=("--db_port=")
-    local_nonpersistent_flags+=("--db_port=")
-    flags+=("--db_ssl_mode=")
-    local_nonpersistent_flags+=("--db_ssl_mode=")
-    flags+=("--db_timeout=")
-    local_nonpersistent_flags+=("--db_timeout=")
-    flags+=("--db_user=")
-    local_nonpersistent_flags+=("--db_user=")
+    flags+=("--db-conn-max-lt=")
+    local_nonpersistent_flags+=("--db-conn-max-lt=")
+    flags+=("--db-idle-conn=")
+    local_nonpersistent_flags+=("--db-idle-conn=")
+    flags+=("--db-open-conn=")
+    local_nonpersistent_flags+=("--db-open-conn=")
+    flags+=("--dsn=")
+    local_nonpersistent_flags+=("--dsn=")
+    flags+=("--grpc-host=")
+    local_nonpersistent_flags+=("--grpc-host=")
+    flags+=("--host=")
+    two_word_flags+=("-H")
+    local_nonpersistent_flags+=("--host=")
     flags+=("--idle-timeout=")
     local_nonpersistent_flags+=("--idle-timeout=")
-    flags+=("--port=")
-    local_nonpersistent_flags+=("--port=")
+    flags+=("--monitoring-host=")
+    local_nonpersistent_flags+=("--monitoring-host=")
+    flags+=("--profiling-host=")
+    local_nonpersistent_flags+=("--profiling-host=")
     flags+=("--read-header-timeout=")
     local_nonpersistent_flags+=("--read-header-timeout=")
     flags+=("--read-timeout=")
@@ -347,8 +546,8 @@ _form-api_run()
     local_nonpersistent_flags+=("--tpl-dir=")
     flags+=("--with-monitoring")
     local_nonpersistent_flags+=("--with-monitoring")
-    flags+=("--with-profiler")
-    local_nonpersistent_flags+=("--with-profiler")
+    flags+=("--with-profiling")
+    local_nonpersistent_flags+=("--with-profiling")
     flags+=("--write-timeout=")
     local_nonpersistent_flags+=("--write-timeout=")
 
@@ -360,6 +559,9 @@ _form-api_run()
 _form-api_version()
 {
     last_command="form-api_version"
+
+    command_aliases=()
+
     commands=()
 
     flags=()
@@ -377,8 +579,12 @@ _form-api_version()
 _form-api_root_command()
 {
     last_command="form-api"
+
+    command_aliases=()
+
     commands=()
     commands+=("completion")
+    commands+=("ctl")
     commands+=("migrate")
     commands+=("run")
     commands+=("version")
@@ -399,6 +605,7 @@ __start_form-api()
 {
     local cur prev words cword
     declare -A flaghash 2>/dev/null || :
+    declare -A aliashash 2>/dev/null || :
     if declare -F _init_completion >/dev/null 2>&1; then
         _init_completion -s || return
     else
