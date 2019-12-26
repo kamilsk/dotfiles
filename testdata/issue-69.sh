4ed70e13515684269271abae213d5180831b1f09
Kamil Samigullin <kamil@samigullin.info>
2019-12-27 00:03:11 +0300

    fix #70: handle unbound variable

--- testdata/issue-69.sh
+++ testdata/issue-69.sh
@@ -5,15 +5,15 @@ set -o pipefail
 
 while IFS= read -r latest; do
 
-    if [[ ${latest} =~ -[0-9]+-g[a-z0-9]{7}$ ]]; then
+    if [[ "${latest}" =~ -[0-9]+-g[a-z0-9]{7}$ ]]; then
         ahead=$(echo ${latest} | rev | cut -d - -f2-2 | rev)
         latest=$(echo ${latest} | rev | cut -d - -f3- | rev)
     fi
-    if [[ ${latest} =~ - ]]; then
+    if [[ "${latest}" =~ - ]]; then
         latest=$(echo ${latest} | sed 's/-/ /' | awk '{print $1" ["$2"]"}')
     fi
-    if [[ ${ahead} ]]; then
-        latest=$(echo -e ${latest} ${ahead}↑)
+    if [[ "${ahead-}" ]]; then
+        latest=$(echo ${latest} ${ahead}↑)
     fi
     echo ${latest}
 
