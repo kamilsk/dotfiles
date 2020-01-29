f173aa23a667ddb0af1462b2d27b30276bf1abaa
Kamil Samigullin <kamil@samigullin.info>
2020-01-29 23:31:37 +0300

    fix #73: add gopath alias

--- testdata/issue-69.sh
+++ testdata/issue-69.sh
@@ -3,7 +3,17 @@
 set -o nounset
 set -o pipefail
 
-while IFS= read -r latest; do
+declare -a arr=(
+    '0.5.5-4-gcaf34db'
+    '0.5.5-alpha-4-gcaf34db'
+    '0.5.0-8-gcfce164'
+    '0.5.0-beta-8-gcfce164'
+    '1.11.0-1-ga4bc8a5'
+    '1.11.0-rc1-1-ga4bc8a5'
+    '0.7.0-166-g4d85c5c5'
+)
+
+for latest in "${arr[@]}"; do
 
     if [[ "${latest}" =~ -[0-9]+-g[a-z0-9]+$ ]]; then
         ahead=$(echo ${latest} | rev | cut -d - -f2-2 | rev)
---
+++ testdata/issue-73.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o pipefail
+
+declare -a arr=(
+    'https://github.com/kamilsk/retry.git'
+    'git@github.com:kamilsk/retry.git'
+    'https://kamilsk@bitbucket.org/kamilsk/retry.git'
+    'git@bitbucket.org:kamilsk/retry.git'
+    'ssh://git@stash.msk.avito.ru:7999/gl/item-aggregator.git'
+)
+
+for package in "${arr[@]}"; do
+
+    package=${package/%.git}
+    package=${package/#https:\/\/}
+    package=${package/#*@}
+
+    # use `declare -A map` in Bash 4 instead    
+    package=${package/stash.msk.avito.ru:7999/go.avito.ru}
+
+    package=${package/:/\/}
+    echo ${package}
+
+done
