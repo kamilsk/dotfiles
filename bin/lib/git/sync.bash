694fd35ceb8c5210410dcc00db69cccb5d808c31
Kamil Samigullin <kamil@samigullin.info>
2022-02-25 20:20:55 +0300

    fix #509: exclude forks from git push --force-with-lease

--- bin/lib/git/sync.bash
+++ bin/lib/git/sync.bash
@@ -14,7 +14,7 @@ git-push() {
   local branch
   branch=$(git branch | cut -f2 -d' ' | awk 'NF > 0')
 
-  for remote in $(git remote | grep -Ev '^(upstream|fork-)'); do
-    git push "${remote}" "${branch}" --tags
+  for remote in $(git remote | grep -Ev '^(upstream|fork-.*)$'); do
+    git push --tags "${remote}" "${branch}"
   done
 }
