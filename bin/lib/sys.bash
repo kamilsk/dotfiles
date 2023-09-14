f64d84f3caefb49b6e5f3f9724385d4b598446fd
Kamil Samigullin <kamil@samigullin.info>
2023-09-14 10:14:28 +0300

    fix #618: command: add books as the same as obsidian

--- bin/lib/sys.bash
+++ bin/lib/sys.bash
@@ -61,6 +61,15 @@ explain() { open "https://explainshell.com/explain?cmd=${*}"; }
 #end
 
 #include:bin/lib/sys/path.bash|uncomment
+books() {
+  cd "$(
+    gum choose \
+      "${HOME}/Library/Mobile Documents/iCloud~com~apple~iBooks/Documents" \
+      "${HOME}/Library/Containers/com.apple.BKAgentService/Data/Documents/iBooks/Books/Audiobooks"
+  )" || return 1
+  du -h -- * | sort -h
+}
+
 obsidian() { cd "${HOME}/Library/Mobile Documents/iCloud~md~obsidian/Documents" || return 1; }
 
 tap() { cd "$(brew --prefix)/Library/Taps/${1:-}" || return 1; }
--- bin/lib/sys/path.bash
+++ bin/lib/sys/path.bash
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+books() {
+  cd "$(
+    gum choose \
+      "${HOME}/Library/Mobile Documents/iCloud~com~apple~iBooks/Documents" \
+      "${HOME}/Library/Containers/com.apple.BKAgentService/Data/Documents/iBooks/Books/Audiobooks"
+  )" || return 1
+  du -h -- * | sort -h
+}
+
 obsidian() { cd "${HOME}/Library/Mobile Documents/iCloud~md~obsidian/Documents" || return 1; }
 
 tap() { cd "$(brew --prefix)/Library/Taps/${1:-}" || return 1; }
